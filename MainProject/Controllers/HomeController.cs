51bdc7338961b1047f6bc5b6c646ca46c8c76c46
raberana <raberana@live.com>
2013-06-15 22:32:19 +0800

    push to github

---
+++ MainProject/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using MainProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MainProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public RedirectToRouteResult Add(User user)
+        {
+            
+            return RedirectToAction("Index", "Home");
+        }
+        public JsonResult Find(User user)
+        {
+            JsonResult jsonData = new JsonResult();
+
+            return Json(jsonData, JsonRequestBehavior.AllowGet);
+        }
+
+        public RedirectToRouteResult Delete(User user)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+
+        public RedirectToRouteResult Edit(User user)
+        {
+
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
