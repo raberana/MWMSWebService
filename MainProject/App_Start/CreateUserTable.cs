36a8685355a6a00b8219e83ee26378c94ea3a617
raberana <raberana@live.com>
2013-07-07 00:40:20 +0800

    added fluent migrator

---
+++ MainProject/App_Start/CreateUserTable.cs
@@ -0,0 +1,29 @@
+﻿using FluentMigrator;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MainProject.App_Start
+{
+    [Migration(1)]
+    public class CreateUserTable : Migration
+    {
+        public override void Up()
+        {
+            if (!Schema.Table("Users").Exists())
+            {
+                Create.Table("Users")
+                    .WithColumn("Id").AsInt32().NotNullable().PrimaryKey().Identity()
+                    .WithColumn("UserName").AsString(255).NotNullable()
+                    .WithColumn("Password").AsString(255).NotNullable()
+                    .WithColumn("ClientId").AsString(255).NotNullable();
+            }
+        }
+
+        public override void Down()
+        {
+            
+        }
+    }
+}
--- MainProject/Global.asax.cs
+++ MainProject/Global.asax.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MainProject.App_Start;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -22,6 +23,9 @@ namespace MainProject
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            var migration = new CreateUserTable();
+            migration.Up();
         }
     }
 }
