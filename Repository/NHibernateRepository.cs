6c1456a848dd3f4f4dcfe8917f949863d7dda735
raberana <raberana@live.com>
2013-07-07 02:17:11 +0800

    changed nhibernate connection string

--- Repository/NHibernateRepository.cs
+++ Repository/NHibernateRepository.cs
@@ -28,7 +28,7 @@ namespace ProjectRepository
                       .Database(
                           MsSqlConfiguration
                           .MsSql2008
-                          .ConnectionString(@"Server=ff4c7673-21c8-4089-93c4-a1e500291e65.sqlserver.sequelizer.com;Database=dbff4c767321c8408993c4a1e500291e65;User ID=dinbhyfbgnktskik;Password=gWQ2FQgQJVudrdeh3LFDvqysvcPgvYiXpFDmiwBzmYCEKjTmAqZgZXdYwwEDbauS;"))
+                          .ConnectionString(@"Data Source=ff4c7673-21c8-4089-93c4-a1e500291e65.sqlserver.sequelizer.com;Initial Catalog=dbff4c767321c8408993c4a1e500291e65;Integrated Security=True;User ID=dinbhyfbgnktskik;Password=gWQ2FQgQJVudrdeh3LFDvqysvcPgvYiXpFDmiwBzmYCEKjTmAqZgZXdYwwEDbauS"))
                           .Mappings(m => m.FluentMappings.AddFromAssemblyOf<TEntity>())
                       .BuildConfiguration();
 
