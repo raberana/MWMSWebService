da86efe43073d16d87c55bdccdb55f73d5020eca
raberana <raberana@live.com>
2013-07-06 21:52:15 +0800

    first stable build

--- MainProject/Scripts/app.js
+++ MainProject/Scripts/app.js
@@ -1,23 +1,20 @@
 ﻿(function ($) {
 
-	$('#verifyBtn').click(function () {
-
-		$.ajax({
-			url: "/Home/Find",
-			type: "POST",
-			data: { userName: $('#txtUserName').val(), password: $('#txtPassword').val() },
-			success: function (data) {
-				alert(JSON.stringify(data));
-				var returnedData = jQuery.parseJSON((JSON.stringify(data)));
-				if (returnedData.ClientId != null && returnedData.ClientId != "")
-				{
-					alert(returnedData.ClientId);
-				}
-				
-				
-			}
-		});
-
-	});
+    $('#verifyBtn').click(function () {
+        $.ajax({
+            url: "/Home/Find",
+            type: "POST",
+            data: { userName: $('#txtUserName').val(), password: $('#txtPassword').val() },
+            success: function (data) {
+                var stringified = JSON.stringify(data);
+                var returnedData = jQuery.parseJSON(stringified.substring(1, stringified.length - 1));
+                if (returnedData.ClientId != null && returnedData.ClientId != "") {
+                    $('#clientId').text(returnedData.ClientId);
+                }
+                else
+                    $('#clientId').text("USER DOES NOT EXIST");
+            }
+        });
+    });
 
 })(jQuery);
