2d3c4826600da999a8631e3d0fdb10f94dd2a85f
roj <raberana@live.com>
2013-08-12 23:44:01 +0800

    add

---
+++ MwmsBusiness/Entities/UserInfo.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MwmsBusiness
+{
+    public class UserInfo
+    {
+        public virtual int Id { get; set; }
+        public virtual string FullName { get; set; }
+        public virtual string Location { get; set; }
+    }
+}
